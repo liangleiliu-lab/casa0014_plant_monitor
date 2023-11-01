5f5828264abaa7fd83eb435fea0b8f9ba7ed8f0d
liangleiliu-lab <70188187+liangleiliu-lab@users.noreply.github.com>
2023-11-01 00:43:57 +0000

    Update arduino_sercert.h

--- arduino_sercert.h
+++ arduino_sercert.h
@@ -1,4 +1,4 @@
-const char* ssid     = CE-Hub-Student;
-const char* password = casa-ce-gagarin-public-service;
-const char* mqttuser = student;
-const char* mqttpass = ce2021-mqtt-forget-whale;
+const char* ssid     = xxx;
+const char* password = xxx;
+const char* mqttuser = xxx;
+const char* mqttpass = xxx;
